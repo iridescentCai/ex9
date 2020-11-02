a9ddc1ce64705e56f4ba48ff94dd7a828214fc25
FFLeiFF <irenevineyard@gmail.com>
2020-11-02 09:10:29 -0800

    1

--- src/main/java/unpair/ZeroOneStrings.java
+++ src/main/java/unpair/ZeroOneStrings.java
@@ -3,8 +3,49 @@ package unpair;
 public class ZeroOneStrings {
 
     public static int getMinimumPairs(String binaryString) {
-        // TODO: implement this method
-        return -1;
+        if (!binaryString.contains("0") && !binaryString.contains("1")) {
+            return 0;
+        }
+        int count = 0;
+        char begin = binaryString.charAt(0);
+        if (begin == '?') {
+            int index = getFirstNotEqualsQuestionMark(binaryString);
+            if (index % 2 == 0) {
+                begin = binaryString.charAt(index);
+            } else {
+                if (binaryString.charAt(index) == '0') {
+                    begin = '1';
+                } else {
+                    begin = '0';
+                }
+            }
+        }
+        for (int i = 1; i < binaryString.length(); i++) {
+            char currentChar = binaryString.charAt(i);
+            if (currentChar == '?') {
+                if (begin == '1') {
+                    begin = '0';
+                    continue;
+                } else {
+                    begin = '1';
+                    continue;
+                }
+            }
+            if (currentChar == begin) {
+                count++;
+            } else {
+                begin = currentChar;
+            }
+        }
+        return count;
     }
 
+    private static int getFirstNotEqualsQuestionMark(String binaryString) {
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) != '?') {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
